73df1b2dd150676ecb883d63f934613b737c1c84
Kyle Unverferth <kyle@kyleu.com>
2015-03-24 14:00:50 -0600

    Inexplicably shortening responses and verifying first line contains a doctype.

--- app/assets/javascripts/utils/Config.js
+++ app/assets/javascripts/utils/Config.js
@@ -1,6 +1,9 @@
 define(function () {
   "use strict";
   var c = window.scalataireConfig;
+  if(c === undefined) {
+    throw "NoConfigurationException";
+  }
   c.wsUrl = "ws://" + document.location.host + "/websocket";
   return c;
 });
