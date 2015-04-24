7bc249413b91a5b3bbaa57d55b364153a59f9513
Kyle Unverferth <kyle@kyleu.com>
2015-04-23 23:57:17 -0600

    Electron scaffolding, moving activator.

---
+++ electron/main.js
@@ -0,0 +1,174 @@
+var app = require('app');
+var Menu = require('menu');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+var menu = null;
+
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: true,
+    'auto-hide-menu-bar': true,
+    'use-content-size': true
+  });
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.focus();
+
+  if (process.platform == 'darwin') {
+    var osxTemplate = [
+      {
+        label: 'Scalataire',
+        submenu: [
+          {
+            label: 'About Scalataire',
+            selector: 'orderFrontStandardAboutPanel:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Services',
+            submenu: []
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Hide Scalataire',
+            accelerator: 'Command+H',
+            selector: 'hide:'
+          },
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            selector: 'hideOtherApplications:'
+          },
+          {
+            label: 'Show All',
+            selector: 'unhideAllApplications:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() { app.quit(); }
+          }
+        ]
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'Command+Z',
+            selector: 'undo:'
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+Command+Z',
+            selector: 'redo:'
+          }
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Command+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: 'Enter Fullscreen',
+            click: function() { mainWindow.setFullScreen(true); }
+          },
+          {
+            label: 'Toggle DevTools',
+            accelerator: 'Alt+Command+I',
+            click: function() { mainWindow.toggleDevTools(); }
+          }
+        ]
+      },
+      {
+        label: 'Window',
+        submenu: [
+          {
+            label: 'Minimize',
+            accelerator: 'Command+M',
+            selector: 'performMiniaturize:'
+          },
+          {
+            label: 'Close',
+            accelerator: 'Command+W',
+            selector: 'performClose:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Bring All to Front',
+            selector: 'arrangeInFront:'
+          }
+        ]
+      },
+      {
+        label: 'Help',
+        submenu: []
+      }
+    ];
+
+    menu = Menu.buildFromTemplate(osxTemplate);
+    Menu.setApplicationMenu(menu);
+  } else {
+    var otherTemplate = [
+      {
+        label: '&File',
+        submenu: [
+          {
+            label: '&Open',
+            accelerator: 'Ctrl+O'
+          },
+          {
+            label: '&Close',
+            accelerator: 'Ctrl+W',
+            click: function() { mainWindow.close(); }
+          }
+        ]
+      },
+      {
+        label: '&View',
+        submenu: [
+          {
+            label: '&Reload',
+            accelerator: 'Ctrl+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: '&Enter Fullscreen',
+            click: function() { mainWindow.setFullScreen(true); }
+          },
+          {
+            label: '&Toggle DevTools',
+            accelerator: 'Alt+Ctrl+I',
+            click: function() { mainWindow.toggleDevTools(); }
+          }
+        ]
+      },
+      {
+        label: '&Help',
+        submenu: []
+      }
+    ];
+
+    menu = Menu.buildFromTemplate(otherTemplate);
+    mainWindow.setMenu(menu);
+  }
+});
