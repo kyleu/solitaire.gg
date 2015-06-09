84a5e51454f155356de2559c18d77154ddc81f25
Kyle Unverferth <kyle@kyleu.com>
2015-06-09 00:43:00 -0600

    Better card removal checks.

--- shared/src/main/scala/models/game/rules/CardRemovalMethod.scala
+++ shared/src/main/scala/models/game/rules/CardRemovalMethod.scala
@@ -20,32 +20,36 @@ object CardRemovalMethod {
     override def canRemove(l: Card, r: Card) = l.s == r.s
   }
   case object RemoveNinesOrPairsAddingToNineOr10JQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(9, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(9, l.r, r.r) // TODO 10-J-Q-K
     override def canSelect(c: Card) = c.r == Rank.Nine
   }
   case object RemovePairsAddingToTenOr10PairOrJPairOrQPairOrKPair extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(10, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(10, l.r, r.r) || {
+      l.r == r.r && (l.r == Rank.Ten || l.r == Rank.Jack || l.r == Rank.Queen || l.r == Rank.King)
+    }
   }
   case object RemovePairsAddingToTenOr10JQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(10, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(10, l.r, r.r) // TODO 10-J-Q-K
   }
   case object RemovePairsAddingToTenOrFour10JQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(10, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(10, l.r, r.r) // TODO 10-J-Q-K
   }
   case object RemovePairsAddingToElevenOrJPairOrQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r) || (l.r == Rank.Jack && r.r == Rank.Jack) // TODO Q-K
   }
   case object RemovePairsAddingToElevenOrJPairOrQPairOrKPair extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r) || {
+      l.r == r.r && (l.r == Rank.Jack || l.r == Rank.Queen || l.r == Rank.King)
+    }
   }
   case object RemovePairsAddingToElevenOrJQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r) // TODO J-Q-K
   }
   case object RemoveSameSuitPairsAddingToElevenOrJQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(11, l.r, r.r) && l.s == r.s // TODO J-Q-K
   }
   case object RemovePairsAddingToTwelveOrQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(12, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(12, l.r, r.r) // TODO Q-K
   }
   case object RemovePairsAddingToThirteenOrK extends CardRemovalMethod {
     override def canRemove(l: Card, r: Card) = checkTotal(13, l.r, r.r)
@@ -55,34 +59,34 @@ object CardRemovalMethod {
     override def canRemove(l: Card, r: Card) = checkTotal(14, l.r, r.r)
   }
   case object RemoveSetsAddingToFifteenOr10JQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(15, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(15, l.r, r.r) // TODO 10-J-Q-K
   }
   case object RemoveSetsAddingToFifteenOrFour10JQK extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(15, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(15, l.r, r.r) // TODO 10-J-Q-K
   }
   case object RemovePairsAddingToFifteenOrAPair extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(15, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(15, l.r, r.r) || (l.r == r.r)
   }
   case object RemovePairsAddingToSeventeenOrA23 extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(17, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(17, l.r, r.r) // TODO A-2-3
   }
   case object RemoveSetsOfOneFaceCardAnd3ThatAddToEighteen extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = checkTotal(18, l.r, r.r)
+    override def canRemove(l: Card, r: Card) = checkTotal(18, l.r, r.r) // TODO Any three adding to 18
   }
   case object RemoveConsecutiveRankPairs extends CardRemovalMethod {
     override def canRemove(l: Card, r: Card) = l.r.value == (r.r.value + 1) || l.r.value == (r.r.value - 1)
   }
   case object RemoveConsecutiveRankPairsOrAK extends CardRemovalMethod {
     override def canRemove(l: Card, r: Card) = {
-      l.r.value == (r.r.value + 1) || l.r.value == (r.r.value - 1) || (l.r == Rank.King && r.r == Rank.Ace) || (l.r == Rank.Ace && r.r == Rank.King)
+      l.r.value == (r.r.value + 1) || l.r.value == (r.r.value - 1) // TODO A-K
     }
   }
   case object RemoveConsecutiveOrEqualRankPairs extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = false
+    override def canRemove(l: Card, r: Card) = l.r == r.r || l.r.value == (r.r.value + 1) || l.r.value == (r.r.value - 1)
   }
 
   case object StackSameRankOrSuitInWaste extends CardRemovalMethod {
-    override def canRemove(l: Card, r: Card) = false
+    override def canRemove(l: Card, r: Card) = false // TODO Who knows
   }
 
   def checkTotal(i: Int, l: Rank, r: Rank, aceHigh: Boolean = false) = {
