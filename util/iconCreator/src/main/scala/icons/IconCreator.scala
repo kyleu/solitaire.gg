cb80b666c6b8857c99a81792e32032faa93f7c6f
Kyle Unverferth <kyle@kyleu.com>
2015-06-13 10:25:17 -0600

    Icon and style improvements, Firefox fixes.

--- util/iconCreator/src/main/scala/icons/IconCreator.scala
+++ util/iconCreator/src/main/scala/icons/IconCreator.scala
@@ -80,6 +80,11 @@ object IconCreator extends App {
       s"convert -resize 64x64 -background none -trim ./tmp/icons/$icon-$color.svg ./tmp/icons/$icon-$color@2x.png".!
       s"convert -resize 32x32 -background none -trim ./tmp/icons/$icon-$color.svg ./tmp/icons/$icon-$color.png".!
     }
+    if(icon == "hint") {
+      s"convert -extent 500x500 -background none ./tmp/icons/$icon-$color-500.png ./tmp/icons/$icon-$color-500.png".!
+      s"convert -extent 64x64 -background none ./tmp/icons/$icon-$color@2x.png ./tmp/icons/$icon-$color@2x.png".!
+      s"convert -extent 32x32 -background none ./tmp/icons/$icon-$color.png ./tmp/icons/$icon-$color.png".!
+    }
   }
 
   private[this] def stitch(color: String) = {
