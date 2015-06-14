ef45c6da10a07145b715bc8fb8001a2e5f3a1a9c
Kyle Unverferth <kyle@kyleu.com>
2015-06-14 13:13:48 -0600

    Electron improvements, custom binaries for OS X and windows.
    Former-commit-id: dd9ac9e604b08898de1d185b907ebfb20cb3b7f1

--- bin/design-desktop-builder.sh
+++ bin/design-desktop-builder.sh
@@ -13,7 +13,7 @@ convert original/icon.png -resize 512x512 osx/icon_256x256@2x.png
 convert original/icon.png -resize 256x256 osx/icon_256x256.png
 convert original/icon.png -resize 256x256 osx/icon_128x128@2x.png
 convert original/icon.png -resize 128x128 osx/icon_128x128.png
-convert original/icon.png -resize 64x64 osx/icon._32x32@2x.png
+convert original/icon.png -resize 64x64 osx/icon_32x32@2x.png
 convert original/icon.png -resize 32x32 osx/icon_32x32.png
 convert original/icon.png -resize 32x32 osx/icon_16x16@2x.png
 convert original/icon.png -resize 16x16 osx/icon_16x16.png
