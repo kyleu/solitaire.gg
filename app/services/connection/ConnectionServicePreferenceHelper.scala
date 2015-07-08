c562fb659b66b87b4eb40da7c904b4e6a348f5da
Kyle Unverferth <kyle@kyleu.com>
2015-07-08 00:19:22 -0600

    Game options UI and images.
    Former-commit-id: d008a63d61907372e7d87c83144f6550c1bb97e2

--- app/services/connection/ConnectionServicePreferenceHelper.scala
+++ app/services/connection/ConnectionServicePreferenceHelper.scala
@@ -10,13 +10,13 @@ import utils.metrics.InstrumentedActor
 trait ConnectionServicePreferenceHelper extends InstrumentedActor { this: ConnectionService =>
   protected[this] def handleSetPreference(sp: SetPreference): Unit = {
     val newPrefs = sp.name match {
-      case "card-back" => user.preferences.copy(cards = user.preferences.cards.copy(back = sp.value))
-      case "card-layout" => user.preferences.copy(cards = user.preferences.cards.copy(layout = sp.value))
-      case "card-rank" => user.preferences.copy(cards = user.preferences.cards.copy(ranks = sp.value))
-      case "card-suit" => user.preferences.copy(cards = user.preferences.cards.copy(suits = sp.value))
-      case "card-face" => user.preferences.copy(cards = user.preferences.cards.copy(faceCards = sp.value))
-      case "auto-flip" => user.preferences.copy(autoFlip = sp.value.toBoolean)
-      case "background-color" => user.preferences.copy(color = sp.value)
+      case "card-back" => userPreferences.copy(cards = userPreferences.cards.copy(back = sp.value))
+      case "card-layout" => userPreferences.copy(cards = userPreferences.cards.copy(layout = sp.value))
+      case "card-rank" => userPreferences.copy(cards = userPreferences.cards.copy(ranks = sp.value))
+      case "card-suit" => userPreferences.copy(cards = userPreferences.cards.copy(suits = sp.value))
+      case "card-face" => userPreferences.copy(cards = userPreferences.cards.copy(faceCards = sp.value))
+      case "auto-flip" => userPreferences.copy(autoFlip = sp.value.toBoolean)
+      case "background-color" => userPreferences.copy(color = sp.value)
       case _ => log.errorThenThrow(s"Unhandled preference [${sp.name}] for user [$user.id] with value [${sp.value}].")
     }
     if (newPrefs != userPreferences) {
