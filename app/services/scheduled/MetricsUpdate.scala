05f0b5e17714434eb09eef3598c363eadd2bf05c
Kyle Unverferth <kyle@kyleu.com>
2015-07-25 14:21:59 -0600

    Metrics and script fixes.
    Former-commit-id: 241fcf94dbc0984da1326caeb467313dba695a20

--- app/services/scheduled/MetricsUpdate.scala
+++ app/services/scheduled/MetricsUpdate.scala
@@ -9,17 +9,18 @@ object MetricsUpdate {
     val d = DateUtils.today
     DailyMetricService.getMetrics(d).flatMap { oldMetrics =>
       DailyMetricService.recalculateMetrics(d).map { newMetrics =>
-        if(oldMetrics == newMetrics) {
+        val differences = newMetrics._2._1.toSeq.flatMap { newMetric =>
+          val oldMetric = oldMetrics._2._1.getOrElse(newMetric._1, 0L)
+          if(oldMetric == newMetric._2) {
+            None
+          } else {
+            Some(newMetric._1.title -> (newMetric._2 - oldMetric))
+          }
+        }
+
+        if(differences.isEmpty) {
           "metrics" -> None
         } else {
-          val differences = newMetrics._2._1.toSeq.flatMap { newMetric =>
-            val oldMetric = oldMetrics._2._1.getOrElse(newMetric._1, 0L)
-            if(oldMetric == newMetric._2) {
-              None
-            } else {
-              Some(newMetric._1.title -> (newMetric._2 - oldMetric))
-            }
-          }
           "metrics" -> Some(s"Updated metrics for [$d], adding [${differences.map(x => x._2 + " " + x._1).mkString(", ")}].")
         }
       }
