e2112c95f23b8abce96af40f1a0a39d0c8236c44
Kyle Unverferth <kyle@kyleu.com>
2015-10-03 13:01:01 -0600

    Allow query param to override rules.
    Former-commit-id: 5bfb67b95dda8805109aae32ae79615d718bb0e1

--- app/assets/javascripts/utils/Config.js
+++ app/assets/javascripts/utils/Config.js
@@ -20,5 +20,25 @@ define(function () {
     cfg.assetRoot = 'assets/';
   }
 
+  var qs = (function(a) {
+    if(a === '') {
+      return {};
+    }
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+      var p=a[i].split('=', 2);
+      if(p.length === 1) {
+        b[p[0]] = '';
+      } else {
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
+      }
+    }
+    return b;
+  })(window.location.search.substr(1).split('&'));
+
+  if(qs.game !== undefined) {
+    cfg.rules = qs.game;
+  }
+
   return cfg;
 });
