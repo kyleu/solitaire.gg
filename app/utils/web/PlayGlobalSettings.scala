e239e3011b48cd020d0afd373dbb44f370f6cdc5
Kyle Unverferth <kyle@kyleu.com>
2015-11-04 19:05:24 -0700

    Removing crap redirect.
    Former-commit-id: cb3f191c46cb621ca91d4dec3c664fcb5d0d9754

--- app/utils/web/PlayGlobalSettings.scala
+++ app/utils/web/PlayGlobalSettings.scala
@@ -1,8 +1,7 @@
 package utils.web
 
 import play.api.GlobalSettings
-import play.api.http.HeaderNames
-import play.api.mvc.{ Action, RequestHeader, Results, WithFilters }
+import play.api.mvc.{ RequestHeader, WithFilters }
 import play.filters.gzip.GzipFilter
 import utils.Logging
 
@@ -14,15 +13,6 @@ object PlayGlobalSettings extends WithFilters(PlayLoggingFilter, new GzipFilter(
     if (!Option(request.path).exists(_.startsWith("/assets"))) {
       log.info(s"Request from [${request.remoteAddress}]: ${request.toString()}")
     }
-    if (request.domain == hostname) {
-      super.onRouteRequest(request)
-    } else {
-      Some(redirectToBareDomain(request))
-    }
-  }
-
-  private[this] def redirectToBareDomain(request: RequestHeader) = Action {
-    val protocol = if (request.secure) { "https" } else { "http" }
-    Results.MovedPermanently(s"$protocol://$hostname${request.path}").withHeaders(HeaderNames.CACHE_CONTROL -> "public, max-age=31556926")
+    super.onRouteRequest(request)
   }
 }
