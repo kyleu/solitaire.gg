fb0c7e151cfb45d1d94f72dd05004d138a7232c6
Kyle Unverferth <kyle@kyleu.com>
2016-01-09 16:08:44 -0700

    Fixed the pending analytics routine.
    Former-commit-id: f995bc481d34e76a882084f77431157e7032c107

--- client/src/main/scala/AjaxHelper.scala
+++ client/src/main/scala/AjaxHelper.scala
@@ -13,8 +13,14 @@ trait AjaxHelper {
   import upickle.default._
 
   private[this] val st = org.scalajs.dom.localStorage
+  private[this] var pendingEvents = Option(st.getItem("pending-events")) match {
+    case Some(pe) => read[Seq[PendingEvent]](pe)
+    case None => Seq.empty[PendingEvent]
+  }
 
-  protected def sendPendingEvents() = getPendingEvents.headOption.foreach { pe =>
+  private[this] var requestInFlight = false
+
+  protected def sendPendingEvents() = pendingEvents.headOption.foreach { pe =>
     sendNetworkPost(pe.path, pe.body, Some(pe.id))
   }
 
@@ -24,54 +30,48 @@ trait AjaxHelper {
   }
 
   protected[this] def sendNetworkPost(path: String, body: String, savedId: Option[UUID] = None): Unit = {
-    val url = urlBase + path
-    val timeout = 20 * 1000 // ms
-    val headers = Map(
-      "Content-Type" -> "application/json",
-      "Accept" -> "application/json"
-    )
+    if (!requestInFlight) {
+      val url = urlBase + path
+      val timeout = 20 * 1000 // ms
+      val headers = Map(
+        "Content-Type" -> "application/json",
+        "Accept" -> "application/json"
+      )
 
-    val f = Ajax("post", url, body, timeout, headers, withCredentials = true, "json")
-    f.onSuccess {
-      case x => savedId.foreach { id =>
-        val newPendingEvents = removePendingEvent(id)
-        newPendingEvents.headOption.foreach(pe => sendNetworkPost(pe.path, pe.body, Some(pe.id)))
+      requestInFlight = true
+      val f = Ajax("post", url, body, timeout, headers, withCredentials = true, "json")
+      f.onSuccess {
+        case x =>
+          requestInFlight = false
+          savedId.foreach { id =>
+            pendingEvents = pendingEvents.filterNot(_.id == id)
+            savePendingEvents()
+          }
+          sendPendingEvents()
       }
-    }
-    f.onFailure {
-      case x => savedId match {
-        case Some(id) => incrementFailureCount(id)
-        case None => savePendingEvent(path, body)
+      f.onFailure {
+        case x =>
+          requestInFlight = false
+          savedId match {
+            case Some(id) => incrementFailureCount(id)
+            case None => savePendingEvent(path, body)
+          }
       }
-    }
-  }
-
-  protected[this] def logPendingEvent() = {
-    val evts = getPendingEvents
-    evts.headOption.foreach { evt =>
-      sendNetworkPost(evt.path, evt.body, Some(evt.id))
     }
   }
 
   private[this] def savePendingEvent(path: String, body: String) = {
-    savePendingEvents(getPendingEvents :+ PendingEvent(id = UUID.randomUUID, path = path, body = body))
+    pendingEvents = pendingEvents :+ PendingEvent(id = UUID.randomUUID, path = path, body = body)
+    savePendingEvents()
   }
 
-  private[this] def incrementFailureCount(id: UUID) = savePendingEvents(getPendingEvents.map {
-    case pe if pe.id == id => pe.copy(failureCount = pe.failureCount + 1)
-    case pe => pe
-  })
-
-  private[this] def removePendingEvent(id: UUID) = {
-    val newPendingEvents = getPendingEvents.filter(_.id == id)
-    savePendingEvents(newPendingEvents)
-    newPendingEvents
+  private[this] def incrementFailureCount(id: UUID) = {
+    pendingEvents = pendingEvents.map {
+      case pe if pe.id == id => pe.copy(failureCount = pe.failureCount + 1)
+      case pe => pe
+    }
+    savePendingEvents()
   }
 
-  private[this] def getPendingEvents = Option(st.getItem("pending-events")) match {
-    case Some(pe) => read[Seq[PendingEvent]](pe)
-    case None => Seq.empty[PendingEvent]
-  }
-
-  private[this] def savePendingEvents(events: Seq[PendingEvent]) = st.setItem("pending-events", write(events))
+  private[this] def savePendingEvents() = st.setItem("pending-events", write(pendingEvents))
 }
