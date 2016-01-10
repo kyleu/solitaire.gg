c43157056dbf79e7b9009e1a8a5930bde7a96328
Kyle Unverferth <kyle@kyleu.com>
2016-01-09 18:31:02 -0700

    Analytics export status work.
    Former-commit-id: 06003698ba01d177b57e5bd3f09cb8d0e3776961

--- app/services/sandbox/Scratchpad.scala
+++ app/services/sandbox/Scratchpad.scala
@@ -1,28 +1,13 @@
 package services.sandbox
 
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import services.analytics.AnalyticsExport
 import utils.ApplicationContext
-import utils.DateUtils._
+
+import scala.concurrent.Future
 
 object Scratchpad extends SandboxTask {
   override def id = "scratchpad"
   override def description = "A one-off I don't feel like putting anwhere else."
   override def run(ctx: ApplicationContext) = {
-    val export = new AnalyticsExport(ctx.config)
-    val persistedDates = export.getPersistedDateCounts
-    persistedDates.map { persisted =>
-      val fsd = export.getFileSystemDetails
-
-      val combinedDates = (fsd.map(_._1) ++ persisted.map(_._1)).distinct.sorted
-      val messages = combinedDates.map { d =>
-        val p = persisted.find(_._1 == d)
-        val f = fsd.find(_._1 == d)
-
-        s"$d: ${p.map(_._2)} /  ${f.map(_._2.map(_._2).sum)}"
-      }
-
-      messages.mkString("\n")
-    }
+    Future.successful("Ok")
   }
 }
