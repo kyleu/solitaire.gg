62622d638f86140617ee8d4028f82d5a83c25c55
Kyle Unverferth <kyle@kyleu.com>
2016-01-29 09:18:56 -0700

    More analytics reporting.
    Former-commit-id: 9625299d023b919076d123e89ea0e57552c5180d

--- app/controllers/admin/AnalyticsFileController.scala
+++ app/controllers/admin/AnalyticsFileController.scala
@@ -1,6 +1,7 @@
 package controllers.admin
 
 import controllers.BaseController
+import org.joda.time.LocalDate
 import services.analytics.AnalyticsFileService
 import utils.ApplicationContext
 
@@ -11,11 +12,31 @@ class AnalyticsFileController @javax.inject.Inject() (override val ctx: Applicat
   val fileService = new AnalyticsFileService(ctx.config)
 
   def overview() = withAdminSession("overview") { implicit request =>
+    import utils.DateUtils._
     Future.successful {
       val ret = fileService.getFileSystemDetails.map { fsd =>
         fsd._1 -> fileService.lineCounts(fsd._1)
-      }
+      }.sortBy(_._1)
       Ok(views.html.admin.analytics.fileOverview(ret))
+    }
+  }
+
+  def trends(key: String) = withAdminSession("trends") { implicit request =>
+    Future.successful {
+      Ok(views.html.admin.analytics.fileTrends(key))
+    }
+  }
+
+  def summary(d: LocalDate) = withAdminSession("summary") { implicit request =>
+    Future.successful {
+      Ok(views.html.admin.analytics.fileDailySummary(d))
+    }
+  }
+
+  def detail(d: LocalDate, key: String) = withAdminSession("detail") { implicit request =>
+    Future.successful {
+      val lines = fileService.getLines(d, key).toSeq
+      Ok(views.html.admin.analytics.fileDetail(d, key, lines))
     }
   }
 }
